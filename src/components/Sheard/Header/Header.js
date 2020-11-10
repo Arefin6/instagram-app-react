3e2dd1d41c63af15028d05731ffeb9bc80a17c10
Arefi6 <arefinhossain3@gmail.com>
2020-11-10 14:52:03 +0600

    headr added

---
+++ src/components/Sheard/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img 
+            className="brand-img"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png" alt=""/>
+        </div>
+    );
+};
+
+export default Header;
